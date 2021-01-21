e7a79283417c842cd76ddbcd7e4cf1759ab0fba4
Fathul Muiin <fmuiin14@gmail.com>
2021-01-21 21:45:11 +0700

    page register laravel

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,7 +1,81 @@
-@extends('layouts.app')
+@extends('layouts.auth')
 
 @section('content')
-<div class="container">
+
+<!-- Contact Section Begin -->
+<section class="contact spad">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 col-md-6 d-flex">
+                <img src="/img/shop.jpg" class="img-responsive">
+            </div>
+
+            <div class="col-lg-4 col-md-4 mx-auto">
+                <div class="contact__content">
+                    <div class="contact__form" id="register">
+                        <h3>Memulai Untuk Jual Beli Dengan Cara Terbaru</h3>
+                        <form action="#">
+                            <div class="form-group">
+                                <label for="nama">Nama</label>
+                                <input type="text" class="form-control" autofocus v-model="name">
+                            </div>
+                            <div class="form-group">
+                                <label for="email">Email</label>
+                                <input type="email" class="form-control" v-model="email">
+                            </div>
+                            <div class="form-group">
+                                <label for="password">Password</label>
+                                <input type="password" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <label for="store">Store</label>
+                                <p class="text-muted">Apakah anda juga ingin membuka toko?</p>
+                                <div class="custom-control custom-radio custom-control-inline">
+                                    <input type="radio" class="custom-control-input" name="is_store_open"
+                                        id="openStoreTrue" v-model="is_store_open" :value="true">
+                                    <label for="openStoreTrue" class="custom-control-label">
+                                        Iya, boleh
+                                    </label>
+                                </div>
+                                <div class="custom-control custom-radio custom-control-inline">
+                                    <input type="radio" class="custom-control-input" name="is_store_open"
+                                        id="openStoreFalse" v-model="is_store_open" :value="false">
+                                    <label for="openStoreFalse" class="custom-control-label">
+                                        Enggak, makasih
+                                    </label>
+                                </div>
+                            </div>
+
+                            <div class="form-group" v-if="is_store_open">
+                                <label for="namatoko">Nama Toko</label>
+                                <input type="text" class="form-control">
+                            </div>
+                            <div class="form-group" v-if="is_store_open">
+                                <label for="category">Kategori</label>
+                                <select name="category" class="form-control">
+                                    <option value="" disabled>Select Category</option>
+                                </select>
+                            </div>
+
+
+                            <a href="#" class="btn btn-success btn-block mt-5">
+                                Sign Up Now
+                            </a>
+                            <a href="#" class="btn btn-signup btn-block mt-2">
+                                Back to Sign In
+                            </a>
+
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Contact Section End -->
+
+
+<div class="container" style="display: none">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -75,3 +149,32 @@
     </div>
 </div>
 @endsection
+
+
+@push('addon-script')
+<script src="/vendor/vue/vue.js"></script>
+<script src="https://unpkg.com/vue-toasted"></script>
+<script>
+    Vue.use(Toasted);
+
+    var register = new Vue({
+        el: '#register',
+        mounted() {
+            this.$toasted.error(
+                "Maaf, tampaknya email sudah terdaftar pada sistem kami.", {
+                    position: "top-center",
+                    className: "rounded",
+                    duration: 1000,
+                }
+            );
+        },
+        data: {
+            name: "Fathul Muii",
+            email: "fmuiin14@gmail.com",
+            password: "",
+            is_store_open: true,
+            store_name: ""
+        }
+    });
+</script>
+@endpush
