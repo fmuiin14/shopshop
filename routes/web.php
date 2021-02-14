752c4216cbefe5bedb5bd394c1247cd11a698285
Fathul Muiin <fmuiin14@gmail.com>
2021-02-14 11:37:56 +0700

    2 urus route start admin page

--- routes/web.php
+++ routes/web.php
@@ -23,16 +23,28 @@ Route::get('/categories', 'CategoryController@index')->name('category');
 
 Route::get('/details/{id}', 'DetailController@index')->name('detail');
 
+
+
+// kalo login
 Route::get('/cart', 'CartController@index')->name('cart');
-
 Route::get('/success', 'CartController@success')->name('success');
-
 Route::get('/register/success', 'Auth\RegisterController@success')->name('register-success');
-
 Route::get('/dashboard', 'DashboardController@index')->name('dashboard');
-
 Route::get('/dashboard/products', 'DashboardProductController@index')->name('dashboard-products');
-
 Route::get('/dashboard/products/{id}', 'DashboardProductController@details')->name('dashboard-products-detail');
+
+// buat admin
+Route::prefix('admin')
+    ->namespace('Admin')
+    ->middleware(['auth', 'admin'])
+    ->group(function() {
+        Route::get('/', 'DashboardController@index')->name('admin-dashboard');
+        Route::resource('category', 'CategoryController');
+        Route::resource('user', 'UserController');
+        Route::resource('product', 'ProductController');
+        Route::resource('product-gallery', 'ProductGalleryController');
+        Route::resource('transaction', 'TransactionController');
+    });
+// end admin
 
 Auth::routes();
